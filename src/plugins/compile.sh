eed251f2002645b406f43807ade10df561c315b8
nigel <nigathan@justca.me>
2017-11-23 15:39:34 -0700

    Add files via upload

---
+++ src/plugins/compile.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+if [ ! -d compiled ]; then
+	mkdir compiled
+fi
+
+if [[ "$1" == "--install" ]]; then
+	switchInstall=1
+	shift 1
+fi
+
+if [[ "$1" == "" ]]; then
+    set -- *.cpp
+fi
+
+if [ ! -f "$1" ]; then
+    echo "Nothing to compile . . ."
+    exit 1
+fi
+
+while [ -f "$1" ]; do
+    echo "Compiling '$1' . . ."
+	pluginsrc="$1"
+	pluginhmo="compiled/${pluginsrc%.*}.hmo"
+	g++ -std=c++11 -I ../include -o "${pluginhmo}" ../o/halfmod.o ../o/str_tok.o -shared "${pluginsrc}" -fPIC
+	err=$?
+	if [[ "$err" == "0" ]]; then
+		if [[ "$switchInstall" == "1" ]]; then
+			cp "${pluginhmo}" "../../halfMod/plugins/${pluginsrc%.*}.hmo"
+			if [[ $? == 0 ]]; then
+				echo "Installed . . ."
+			fi
+		fi
+	fi
+	if [[ $err -ne 0 ]]; then
+    	echo "Error compiling '${pluginsrc}': $err"
+	fi
+	shift 1
+done
+
